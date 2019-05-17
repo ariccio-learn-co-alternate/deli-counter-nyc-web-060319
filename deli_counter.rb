f94dd15e240e56965f183b25477f2fe6653a2907
Alexander Riccio <test35965@gmail.com>
2019-05-17 20:34:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,18 @@
 # Write your code here.
 
-def line(katz_deli)
-  if katz_deli.empty?()
+def line(deli_line)
+  if deli_line.empty?()
     puts "The line is currently empty."
     return
   end
   puts "The line is currently:"
   iter = 1
-  katz_deli.each do |person|
+  deli_line.each do |person|
     puts " #{iter}. #{person}"
     iter += 1
   end
+end
+
+def take_a_number(deli_line, name)
+  
 end
